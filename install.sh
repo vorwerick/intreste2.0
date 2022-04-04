9339b45244aecce46f8ee3fde27e54336a3bb792
Aleš Džubera <dzubera@uxf.cz>
2022-04-04 16:57:26 +0200

    update changes

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 cp /home/pi/interste2.0/Intreste.desktop /etc/xdg/autostart
 cp /home/pi/intreste2.0/Intreste.desktop /home/pi/Desktop
--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 java -jar /home/pi/interste2.0/build/compose/jars/Intreste-linux-arm64-2.0.0.jar
