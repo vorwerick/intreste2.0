0fc5b2abf6408cddb6ff80e0d856619198d2ffc5
Aleš Džubera <dzubera@uxf.cz>
2022-04-04 17:21:55 +0200

    update changes

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 sudo rm -rf /etc/xdg/autostart/Intreste.desktop
 sudo rm -rf /home/pi/Desktop/Intreste.desktop
 
-sudo cp /home/pi/interste2.0/Intreste.desktop /etc/xdg/autostart
+sudo cp /home/pi/intreste2.0/Intreste.desktop /etc/xdg/autostart
 sudo cp /home/pi/intreste2.0/Intreste.desktop /home/pi/Desktop
