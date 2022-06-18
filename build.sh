a4809714ce0ac2dd0f7fc95ab7885a2e55224cc4
Aleš Džubera <dzubera@uxf.cz>
2022-06-18 17:33:05 +0200

    fix: distribution scripts

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ cd /home/pi/intreste2.0
 echo "Building application"
 ./gradlew --stop
 ./gradlew packageUberJarForCurrentOS
+sudo chmod +x /home/pi/intreste2.0/run.sh
+sudo chmod +x /home/pi/intreste2.0/build/compose/jars/Intreste-linux-arm64-2.0.0.jar
