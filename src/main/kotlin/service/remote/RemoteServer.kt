c275a32857f451584957186ac1f03149e937e360
Aleš Džubera <dzubera@uxf.cz>
2022-07-29 15:49:59 +0200

    new api and serial communication protocol 2.2.2

--- src/main/kotlin/service/remote/RemoteServer.kt
+++ src/main/kotlin/service/remote/RemoteServer.kt
@@ -133,12 +133,13 @@ class RemoteServer {
     }
 
     fun sendMessage(type: Int, data: JsonObject?): Boolean {
+        val dataString = data?.toJsonString()
         val message = MessageProtocol.Message(
             0,
             type,
             System.currentTimeMillis(),
-            data?.size ?: 0,
-            data?.toJsonString() ?: ""
+            dataString?.length ?: 0,
+            dataString ?: ""
         )
         val packagedMessage = messageProtocol.packMessage(message)
         write(packagedMessage)
